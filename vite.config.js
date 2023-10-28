b3f1214f5bf1b63b9f1895a23ce11f30d2c5fad2
suyungshan <samsys2324@gmail.com>
2023-10-28 13:31:05 +0800

    MusicPlayer (change 5173 to 8000)

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 5173,
+    host: true,
+    strictPort: true,
+    port: 8000,
   },
   plugins: [react()],
 });
